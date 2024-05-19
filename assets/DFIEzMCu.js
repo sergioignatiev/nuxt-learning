fef4ddb2234656ba326f239533b67bac663364d7
SergioIgnatev <ignatevsergey17@gmail.com>
2024-05-19 22:07:18 +0300

    Updates

--- assets/DFIEzMCu.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./5ThHwvKD.js";const t={};function o(r,a){return n(),c("h1",null,"TAILWIND")}const _=e(t,[["render",o]]);export{_ as default};
---
+++ assets/DvFHwpoJ.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,D as o,o as c}from"./BJwE0UoJ.js";const a={},r={class:"default"};function n(e,_){return c(),s("div",r,[o(e.$slots,"default")])}const d=t(a,[["render",n]]);export{d as default};
